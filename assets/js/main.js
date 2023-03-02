0955afdb90188c0ad7deafc9bcce873dabd1aa59
Tanim Mahbub <tanim.mahbub001@gmail.com>
2023-03-03 00:12:17 +0600

    r

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,35 @@
 $(document).ready(function() {
     $('.custom-select').select2();
+    $(".menu-switch").on('click', function () {
+        if($(this).hasClass('is-active')) {
+            $(this).removeClass('is-active');
+        } else {
+            $(this).addClass('is-active');
+        }
+        if($(".nav-main").hasClass('focus')) {
+            $(".nav-main").removeClass('focus');
+        } else {
+            $(".nav-main").addClass('focus');
+        }
+    });
 });
+
+var $navi = $("header"), scrollTop = 0;
+    $(window).scroll(function () {
+    var y = $(this).scrollTop(), speed = 0.05, pos = y * speed, maxPos = 110;
+    if (y > scrollTop) {
+        pos = maxPos;
+    } else {
+        pos = 0;
+    }
+    scrollTop = y;
+    $navi.css({
+        "-webkit-transform": "translateY(-" + pos + "%)",
+        "-moz-transform": "translateY(-" + pos + "%)",
+        "-o-transform": "translateY(-" + pos + "%)",
+        "transform": "translateY(-" + pos + "%)"
+    });
+    });
 
 $('.card-wrapper').simpleLoadMore({
     item: '.greet-card',
@@ -21,4 +50,4 @@ $('.fav').on('click', function () {
 $(".upload-image a").on('click', function(e) {
     e.preventDefault();
     $('#uploadImg').trigger('click'); 
-})
+});
